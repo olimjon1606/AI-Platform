600640043931a42524b3cfff3cc2517f9fbbcaed
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-11-02 17:32:04 +0100

    image section
    image section

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useState } from "react";
 import * as z from "zod";
 import { Heading } from "@/components/heading";
-import { ImageIcon } from "lucide-react";
+import { Download, ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { Form, FormField, FormItem, FormControl } from "@/components/ui/form";
 import { cn } from "@/lib/utils";
@@ -21,6 +21,8 @@ import {
   SelectContent,
   SelectItem,
 } from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const ImagePage = () => {
   const router = useRouter();
@@ -72,16 +74,15 @@ const ImagePage = () => {
                   px-3
                   md:px-6
                   focus-within:shadow-sm
-                  grid-cols-12
+                  grid-cols-6
                   gap-2
                    
-
                   "
             >
               <FormField
                 name="prompt"
                 render={({ field }) => (
-                  <FormItem className="col-span-12 bg-red lg:col-span-6">
+                  <FormItem className="col-span-12 lg:col-span-6">
                     <FormControl className="m-0 p-0">
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
@@ -97,7 +98,7 @@ const ImagePage = () => {
                 control={form.control}
                 name="amount"
                 render={({ field }) => (
-                  <FormItem className="col-span-12 lg:col-span-2">
+                  <FormItem className="col-span-12 lg:col-span-6">
                     <Select
                       disabled={isLoading}
                       onValueChange={field.onChange}
@@ -165,7 +166,25 @@ const ImagePage = () => {
           {images.length === 0 && !isLoading && (
             <Empty label="No images generated." />
           )}
-          <div>Images will be redered here</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+            {images.map((src) => (
+              <Card key={src} className="rounded-lg overflow-hidden">
+                <div className="relative aspect-square">
+                  <Image
+                    fill
+                    alt="Generated"
+                    src={src}
+                  />
+                </div>
+                <CardFooter className="p-2">
+                  <Button onClick={() => window.open(src)} variant="secondary" className="w-full">
+                    <Download className="h-4 w-4 mr-2" />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
